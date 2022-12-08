376ae2443d9741f49c35560cdb32682eddc4b966
RW610 <118364581+RW610@users.noreply.github.com>
2022-12-08 22:11:56 +0300

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,92 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+var width = canvas.width
+var height = canvas.height
+
+var rand = function (x = 1) {
+  return Math.random() * x
+}
+
+let round = function (num = 1, x = 1) {
+  return parseFloat(num.toFixed(x))
+}
+
+randomColor = function (x = 1) {
+  return 'hsla(' + (Math.random() * 360) + ', 50%, 50%, ' + x + ')'
+}
+blackColor = function (x = 1) {
+  return 'hsla(' + (Math.random() * 360) + ', 0%, 0%, ' + x + ')'
+}
+
+class pixel {
+  constructor() {
+    this.size = size
+    this.x = rand(R)
+    this.y = rand(R)
+    this.xSpeed = (rand() - .5) / 4
+    this.ySpeed = (rand() - .5) / 4
+    this.ang = rand(Math.PI * 2)
+    this.color = randomColor(1)
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.size, this.size)
+    ctx.rotate(this.ang)
+  }
+  move() {
+    this.x += this.xSpeed
+    this.y += this.ySpeed
+  }
+  bounce() {
+    if (this.x < 0 || this.x > R) {
+      this.xSpeed = -this.xSpeed
+      ctx.fillStyle = randomColor(a)
+      a *= 5 / 6
+      //this.xSpeed *= .9
+      //ang *= 1.1//rand(.1) - .05
+      this.size *= 10 / 9
+    }
+    if (this.y < 0 || this.y > R) {
+      this.ySpeed = -this.ySpeed
+      ctx.fillStyle = randomColor(a)
+      a *= 5 / 6
+      //this.ySpeed *= .9
+      //ang *= 1.1//rand(.1) - .05
+      this.size *= 10 / 9
+    }
+  }
+}
+
+ctx.translate(540, 540)    //смещает центр вращения
+
+let R = 380
+size = 1
+let a = 1
+let ang = 2 * Math.PI / round(rand(1), 2)                    //угол поворота  rand(Math.PI * 2) 
+let point = new pixel()
+ctx.fillStyle = randomColor(a)
+
+//setInterval(function () {
+while (a > 0.01) {
+  point.draw()
+  point.bounce()
+  point.move()
+
+  //ctx.clearRect(-960, -540, width, height)
+}
+  //a = 1
+//}, 1)
+
+//R = 400
+//a = .5
+//size = 2
+//ang = 2 * Math.PI / round(rand(1), 2)                    //угол поворота  rand(Math.PI * 2)
+//point = new pixel()
+
+//while (a > 0.01) {
+  //ctx.fillStyle = blackColor(a)
+  //point.draw()
+  //point.bounce()
+  //point.move()
+//}
+
+
